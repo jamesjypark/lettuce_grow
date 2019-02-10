dd7dfe63fdbcb4e6975534d65caf64632785fb99
kozr <zxzflow@gmail.com>
2019-02-09 17:24:55 -0800

    Basics

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'footer.css';
+import './App.css';
+
+class Footer extends Component {
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Footer text</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
