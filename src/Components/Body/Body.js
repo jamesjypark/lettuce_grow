d6065c557a889230f1c7b1660061dbfd7c3d8035
Juyoung Park <james.park.victoria@gmail.com>
2019-02-10 04:29:51 -0800

    resolved conflicts

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -11,7 +11,6 @@ class Body extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <div className={this.props.content === "landing" ? "" : "hidden"}>
           <Landing />
         </div>
@@ -25,17 +24,6 @@ class Body extends Component {
         <div className={this.props.content === "search" ? "" : "hidden"}>
           <Search />
         </div>
-=======
-         <div className={this.props.content === "community" ? "" : "hidden"}>
-           <Community />
-         </div>
-         <div className={this.props.content === "grow" ? "" : "hidden"}>
-           <Grow />
-         </div>
-         <div className={this.props.content === "search" ? "" : "hidden"}>
-           <Search />
-         </div>
->>>>>>> 26e17ba89bac56dac9da8b04ced0c9cb0cf2a628
       </div>
     );
   }
