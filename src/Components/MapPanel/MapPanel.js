45539c354664c67c7ac35c26cb517587f6cfe143
Archiving <solarcactus@live.com>
2019-02-09 23:07:32 -0800

    Got markers for regions working!

--- src/Components/MapPanel/MapPanel.js
+++ src/Components/MapPanel/MapPanel.js
@@ -3,13 +3,14 @@ import { GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 // import "./MapPanel.css";
 import { API_KEY } from "../../Constants/apiKey";
 import CurrentLocation from "./CurrentLocation";
+import { FAKE_DATA } from '../../Constants/FAKE_DATA'
 
 export class MapPanel extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {}
-  };
+  };s
 
   onMarkerClick = (props, marker, e) => {
     this.setState({
@@ -30,16 +31,16 @@ export class MapPanel extends Component {
 
   render() {
     return (
+      <div>
+
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-        {positions.map(i => {
-          
-          <Marker onClick={this.onMarkerClick} position={i} name='TEST 1'/>
-        })}
+        {FAKE_DATA.positions.map(
+          location=>(<Marker onClick={this.onMarkerClick} position={location} name={location.name} />))}
         <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
           <h4>{this.state.selectedPlace.name}</h4>
         </InfoWindow>
       </CurrentLocation>
-      
+      </div>
     );
   }
 }
--- src/Constants/FAKE_DATA.js
+++ src/Constants/FAKE_DATA.js
@@ -17,16 +17,21 @@ export const FAKE_DATA = {
   positions: [
     {
       lat: 49.260758,
-      lng: -123.251017
+      lng: -123.251017,
+      name: 'ETC'
     }, {
       lat: 49.264515,
-      lng: -123.258075
+      lng: -123.258075,
+      name: 'XAETC'
     }, {
       lat: 49.264162,
-      lng: -123.249378
+      lng: -123.249378,
+      name: 'AETC'
     }, {
       lat: 49.269200,
-      lng: -123.250636
+      lng: -123.250636,
+      name: 'CETC'
     }
   ]
 }
+
