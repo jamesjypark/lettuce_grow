3186cff0d15d550f11a261caaae0e1c43ae0d7d8
Juyoung Park <james.park.victoria@gmail.com>
2019-02-10 01:38:56 -0800

    Design implemented for Navigation Bar

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <div>This is sample </div>
-
         <Header onHeaderSelected={this.onHeaderSelected} />
         <Body content={this.state.currentDisplay} />
         <Footer />
