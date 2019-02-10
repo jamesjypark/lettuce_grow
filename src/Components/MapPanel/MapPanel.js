e5b9ca636007dc9d777b0b4f5a58e4490370ae37
Juyoung Park <james.park.victoria@gmail.com>
2019-02-09 21:24:38 -0800

    Resolved all conflicts

--- src/Components/MapPanel/MapPanel.js
+++ src/Components/MapPanel/MapPanel.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { GoogleApiWrapper } from 'google-maps-react';
-import './mapPanel.css';
-import {API_KEY} from '../../Constants/apiKey';
+import React, { Component } from "react";
+import { GoogleApiWrapper } from "google-maps-react";
+import "./MapPanel.css";
+import { API_KEY } from "../../Constants/apiKey";
 import CurrentLocation from "./Map";
 
 export class MapPanel extends Component {
-    render() {
-        return (
-            <CurrentLocation
-                centerAroundCurrentLocation 
-                google = {this.props.google} />
-        )
-    }
+  render() {
+    return (
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google} />
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-    apiKey: API_KEY
-  })(MapPanel);
+  apiKey: API_KEY
+})(MapPanel);
