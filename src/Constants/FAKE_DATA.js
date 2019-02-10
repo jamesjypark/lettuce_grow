4f8357b4ceccfc5638c2fda41952013553ba0fde
Brandon Tong Jia Xin <brandonidas@gmail.com>
2019-02-09 23:19:16 -0800

    UPDATE FAKE DATA

--- src/Constants/FAKE_DATA.js
+++ src/Constants/FAKE_DATA.js
@@ -36,25 +36,46 @@ export const FAKE_DATA = {
           progress : 0.6
         }
       ]
+    },
+    {
+      name: "Frank",
+      farmType: "Apartment",
+      region: "A Fun Thyme",
+      crops: [
+        {
+          name :  "Tomato",
+          progress : 0.8
+        }, {
+          name :  "Potato",
+          progress : 0.7
+        }, {
+          name :  "Green Onion",
+          progress : 0.6
+        }
+      ]
     }
   ],
   positions: [
     {
       name: "A Fun Thyme",
       lat: 49.260758,
-      lng: -123.251017
+      lng: -123.251017,
+      members: ['Bob', 'Frank', 'Sam']
     }, {
       name: "Rooting For You",
       lat: 49.264515,
-      lng: -123.258075
+      lng: -123.258075,
+      members: ['Bob', 'Frank', 'Sam']
     }, {
       name: "Raisin The Bar",
       lat: 49.264162,
-      lng: -123.249378
+      lng: -123.249378,
+      members: ['Bob', 'Frank', 'Sam']
     }, {
       name : "Just Beet It",
       lat: 49.269200,
-      lng: -123.250636
+      lng: -123.250636,
+      members: ['Bob', 'Frank', 'Sam']
     }
   ]
 }
