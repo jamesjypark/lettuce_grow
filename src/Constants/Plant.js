fab73ba982f1c7a9ceada927829a62c21a61bfc4
Archiving <solarcactus@live.com>
2019-02-09 18:14:43 -0800

    Downloaded React, Added Google API to MapPanel.

---
+++ src/Constants/Plant.js
@@ -0,0 +1,17 @@
+import TomatoImage from ".../Assets/Tomato.png"
+import EggplantImage from "...Assets/Eggplant.png"
+
+export default plantList = {
+    plantTypes: [Eggplant, Tomato]
+}
+
+const Eggplant = {
+    name: 'Eggplant',
+    image: EggplantImage
+}
+
+const Tomato = {
+    name : "Tomato",
+    image: TomatoImage
+}
+
