f1a1f295e9ca3dece9a65e0fdda96872d8a41d55
Archiving <solarcactus@live.com>
2019-02-09 23:26:04 -0800

    solve merge conflict

--- src/Constants/FAKE_DATA.js
+++ src/Constants/FAKE_DATA.js
@@ -60,38 +60,22 @@ export const FAKE_DATA = {
       name: "A Fun Thyme",
       lat: 49.260758,
       lng: -123.251017,
-<<<<<<< HEAD
       members: ['Bob', 'Frank', 'Sam']
-=======
-      name: 'ETC'
->>>>>>> 1e919a196bc1b375aa6e21b144ae93ed253d9588
     }, {
       name: "Rooting For You",
       lat: 49.264515,
       lng: -123.258075,
-<<<<<<< HEAD
       members: ['Bob', 'Frank', 'Sam']
-=======
-      name: 'XAETC'
->>>>>>> 1e919a196bc1b375aa6e21b144ae93ed253d9588
     }, {
       name: "Raisin The Bar",
       lat: 49.264162,
       lng: -123.249378,
-<<<<<<< HEAD
       members: ['Bob', 'Frank', 'Sam']
-=======
-      name: 'AETC'
->>>>>>> 1e919a196bc1b375aa6e21b144ae93ed253d9588
     }, {
       name : "Just Beet It",
       lat: 49.269200,
       lng: -123.250636,
-<<<<<<< HEAD
       members: ['Bob', 'Frank', 'Sam']
-=======
-      name: 'CETC'
->>>>>>> 1e919a196bc1b375aa6e21b144ae93ed253d9588
     }
   ]
 }
