573289d8d9eb5798a23f7a2b6aae5af337830054
Archiving <solarcactus@live.com>
2019-02-09 22:46:19 -0800

    Added Markers

--- src/Components/MapPanel/MapPanel.js
+++ src/Components/MapPanel/MapPanel.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
-import { GoogleApiWrapper } from "google-maps-react";
+import { GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 // import "./MapPanel.css";
 import { API_KEY } from "../../Constants/apiKey";
 import CurrentLocation from "./CurrentLocation";
 
 export class MapPanel extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onClose = props => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
   render() {
     return (
-      <CurrentLocation 
-      centerAroundCurrentLocation 
-      google={this.props.google} />
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
+        {positions.map(i => {
+          
+          <Marker onClick={this.onMarkerClick} position={i} name='TEST 1'/>
+        })}
+        <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </InfoWindow>
+      </CurrentLocation>
+      
     );
   }
 }
