d2ac5c5822436025ab865eb8873a77698ce6c1e9
Archiving <solarcactus@live.com>
2019-02-10 02:32:02 -0800

    Added new custom window features

--- src/Components/MapPanel/CustomWindow.js
+++ src/Components/MapPanel/CustomWindow.js
@@ -1,18 +1,31 @@
 import React, {Component} from "react";
-
+import "./CustomWindow.css";
 
 export class CustomWindow extends Component {
     constructor(props) {
         super(props);
     }
 
+    buildCrops(crops) {
+        var cropString = [];
+        crops && crops.map(crop => (cropString.push(crop.name)));
+        return cropString.join(", ");
+    }
+
     render() {
         return (<div className="custom_window">
-            <h4 className="title">{'"' + this.props.name + '"' + " Community"}</h4>
-            {this.props.members && <ul>
-                {this.props.members.map(member => (<li>{member.name}</li>))}
-            </ul>}
+            <h4>{'"' + this.props.name + '"' + " Community"}</h4>
+            {this.props.members && this.props.members.map(member => (
+                    <div className="master">
+                        {member.crops != null && <div className="circle" />}
+                        <div className="listitem">
+                            <p className="title">{member.name}</p>
+                            <p className="crops">{this.buildCrops(member.crops)}</p>
+                        </div>
+                    </div>
+                ))}
         </div>);
+
     }
 }
 
