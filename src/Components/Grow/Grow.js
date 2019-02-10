83433b31b70fd4732d2f2afad5dbd459c7abbb12
kozr <zxzflow@gmail.com>
2019-02-10 01:12:15 -0800

    Implemented PlantPanel

--- src/Components/Grow/Grow.js
+++ src/Components/Grow/Grow.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PlantPanel from "../PlantPanel/PlantPanel";
 
+
 class Grow extends Component {
   render() {
     return <div>Grow <h1>Select Your Crops.</h1><PlantPanel /></div>;
